a3bc8db2d2141387fd653c64adeaaf92802fb36c
dayatznur <dayatznur@gmail.com>
2023-08-11 20:50:04 +0700

    setup navigator

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './pages/Home/HomeScreen';
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
