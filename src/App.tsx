67f865ec35da72891bca85e871e3d67d624f9b77
dayatznur <dayatznur@gmail.com>
2023-08-12 19:10:29 +0700

    add ios splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './pages/Home/HomeScreen';
 import SettingScreen from './pages/Setting/SettingScreen';
 import HeaderRight from './components/general/HeaderRight';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <NavigationContainer>
